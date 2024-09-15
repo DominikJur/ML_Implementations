834c8565ef0fc880c9247f4df7be84ce6b3fa45a
DominikJur <282214@student.pwr.edu.pl>
2024-09-15 13:20:55 +0200

    refactoring_2

--- algos/linearmodel.py
+++ algos/linearmodel.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from base import IEstimator
+from algos.base import IEstimator
 
 
 class LinReg(IEstimator):
--- algos/neighbours.py
+++ algos/neighbours.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 import numpy as np
 
-from base import IEstimator
+from algos.base import IEstimator
 
 
 class KNN(IEstimator):
